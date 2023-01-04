cfb9934b7f16a123c965d80803d554bd7df21368
Patryk Jajdyk <89976211+jajdys@users.noreply.github.com>
2023-01-04 11:09:05 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,15 +1,15 @@
-function Sprawdz(){
+function sprawdz(){
     let Login1 = document.getElementById(Login);
     let Haslo1 = document.getElementById(Haslo);
     let ReHaslo1 = document.getElementById(ReHaslo);
 
-    if(Login1=""){
+    if(Login1==null){
         wynik1.innerHTML="Nie podano loginu";
     }
-    if(Haslo1=""){
-        wynik1.innerHTML="Nie podano hasła";
+    if(Haslo1==null){
+        wynik2.innerHTML="Nie podano hasła";
     }
-    if(ReHaslo1=""){
-        wynik1.innerHTML="Nie potwierdzono hasła";
+    if(ReHaslo1==null){
+        wynik3.innerHTML="Nie potwierdzono hasła";
     }
 }
